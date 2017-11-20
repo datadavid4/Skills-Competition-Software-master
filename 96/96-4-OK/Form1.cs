0d00e6483d9d1b1c2a96d019f2503ed1e8c21b46
DavidHsu <davidmd9830415@gmail.com>
2017-11-20 19:48:30 +0800

    update

---
+++ 96/96-4-OK/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _96_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string input = textBox1.Text.Replace(" ", "");
+
+            textBox2.Text = string.Join(" ", postfix(input).ToCharArray());
+        }
+
+        string postfix(string input)
+        {
+            string result = "";
+
+            Stack<string> stack = new Stack<string>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string now = input[i].ToString();
+
+                switch (now)
+                {
+                    case "(":
+                        stack.Push(now);
+                        break;
+
+                    // 右括號輸出優先最高
+                    case ")":
+                        while (stack.Peek() != "(") result += stack.Pop();
+                        stack.Pop();// 推出"("
+                        break;
+
+                    case "+":
+                    case "-":
+                    case "*":
+                    case "/":
+                        int nowPri = getPriority(now);
+                        int lastPre = 0;
+                        if (stack.Count > 0) lastPre = getPriority(stack.Peek());
+                        // 比較當前運算子優先度是否比較大，比較大就堆入，這樣輸出的時候就是優先大的在前面
+                        if (nowPri > lastPre)
+                            stack.Push(now);
+                        else
+                        {
+                            // 如果優先度比較小，像是last = *、now = +，這是後就先輸出*
+                            while (stack.Count > 0)
+                                result += stack.Pop();
+                            stack.Push(now);
+                        }
+                        break;
+
+                    // 數字
+                    default:
+                        result += now;
+                        break;
+                }
+            }
+
+            // 把殘存的輸出
+            while (stack.Count > 0)
+                result += stack.Pop();
+
+
+            return result;
+        }
+
+        int getPriority(string op)
+        {
+            if (op == "*" || op == "/") return 2;
+            else if (op == "+" || op == "-") return 1;
+            return 0;
+        }
+    }
+}
